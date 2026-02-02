fa8db88cf3919a8f34ba554dc3e1b9c6cd6b9937
akash-b-2824 <akashb1152004@gmail.com>
2026-02-02 15:35:33 +0530

    gg

---
+++ ppp.py
@@ -0,0 +1,27 @@
+# Integer
+num_int = 42
+print(type(num_int))  # <class 'int'>
+# Float
+num_float = 3.14
+print(type(num_float))  # <class 'float'>
+# String
+text = "akash"
+print(type(text))  # <class 'str'>
+# Boolean
+flag = True
+print(type(flag))  # <class 'bool'>
+# List
+items = [1, 2, 3]
+print(type(items))  # <class 'list'>
+# Dictionary
+person = {"name": "Akash", "age": 25}
+print(type(person))  # <class 'dict'>
+# Tuple
+coords = (10, 20)
+print(type(coords))  # <class 'tuple'>
+# Set
+unique = {1, 2, 3}
+print(type(unique))  # <class 'set'>
+# None
+empty = None
+print(type(empty))  # <class 'NoneType'>
