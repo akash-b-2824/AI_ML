ba65bcaf083cec217f15379837ef10e36a059191
akash-b-2824 <akashb1152004@gmail.com>
2026-02-03 12:38:21 +0530

    mama mayo

---
+++ day2/bill.py
@@ -0,0 +1,4 @@
+totalamt=int(input("Enter the total bill amount: "))
+n=int(input("Enter number of people: "))
+indiviusalshare=totalamt/n
+print("total bill ",totalamt,"and Each person should pay: ",indiviusalshare)
